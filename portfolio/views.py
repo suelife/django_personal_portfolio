633f769bb964b493e8d6b70393e148b250c16f4f
Wind Kuo <suelife@gmail.com>
2021-12-08 10:01:42 +0800

    ✨ feat: 加入 portfolio app

---
+++ portfolio/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Project
+
+# Create your views here.
+def home(request):
+    projects = Project.objects.all()
+    context = {
+        'title': 'Portfolio.',
+        'projects': projects
+    }
+    return render(request, 'portfolio/home.html', context=context)
